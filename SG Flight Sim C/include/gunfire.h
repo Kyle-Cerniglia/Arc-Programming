84a8c646d9be2794f1e7e525144085a8df6953d7
Netan-MalDoran <kylecerniglia@email.arizona.edu>
2017-03-31 16:48:50 -0700

    Add files via upload

---
+++ SG Flight Sim C/include/gunfire.h
@@ -0,0 +1,14 @@
+#ifndef GUNFIRE_H
+#define GUNFIRE_H
+
+
+class gunfire
+{
+    public:
+        gunfire();
+        virtual ~gunfire();
+    protected:
+    private:
+};
+
+#endif // GUNFIRE_H
---
+++ SG Flight Sim C/include/weaponfireentity.h
@@ -0,0 +1,14 @@
+#ifndef WEAPONFIREENTITY_H
+#define WEAPONFIREENTITY_H
+
+
+class weaponfireentity
+{
+    public:
+        weaponfireentity();
+        virtual ~weaponfireentity();
+    protected:
+    private:
+};
+
+#endif // WEAPONFIREENTITY_H
